780aac5aff371073815e3a4a94bed5eca321afec
Alex <dev.alex.88@gmail.com>
2019-08-04 01:20:31 +0700

    add firebase

---
+++ functions/local.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const server = require('./server')
+
+const port = process.env.PORT || 3000
+
+server.listen(port, err => {
+  if (err) throw err
+  console.log(`Listening on port ${port}`)
+})
--- functions/server.js
+++ functions/server.js
@@ -1,5 +1,3 @@
-require('dotenv').config()
-
 const express = require('express')
 const next = require('next')
 const test = require('./api/test')
@@ -11,18 +9,12 @@ const handle = app.getRequestHandler()
 
 const server = express()
 
-const port = process.env.PORT || 3000
-
 // sequelize.sync().then(() => {
 app.prepare().then(() => {
   server.use(test)
   // handling everything else with Next.js
   server.get('*', handle)
-  server.listen(port, err => {
-    if (err) throw err
-    console.log(`Listening on port ${process.env.PORT}`)
-  })
 })
 // })
 
-module.exports = { server }
+module.exports = server
