a2e2358f5e545cc45d4991870ea863b45b94c11d
Huy Nguyen <huynguyen@FVFY81QKHV2H.local>
2019-08-07 22:09:16 +0700

    issue9: add config fallback to firebase environment configuration

--- functions/config/config.js
+++ functions/config/config.js
@@ -1,8 +1,13 @@
+const functions = require('firebase-functions')
+
+function getFirebaseConfig(key) {
+  return functions.config()[key]
+}
 const dbConfig = {
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'root',
+  username: process.env.DB_USERNAME || functions.config().db.username,
+  password: process.env.DB_PASSWORD || functions.config().db.password,
   database: 'moonie',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || functions.config().db.host,
   dialect: 'postgres',
   define: {
     freezeTableName: true,
