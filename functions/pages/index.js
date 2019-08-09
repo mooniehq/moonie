9ed0c2d5ba30196dfed3b88c3f5276b9b44ebf23
Huy Nguyen <huynguyen@SGH034RY6T.kms.com.vn>
2019-08-09 11:09:18 +0700

    issue12: add Markdown editor

--- functions/pages/index.js
+++ functions/pages/index.js
@@ -1,10 +1,22 @@
+import * as React from "react";
 import Link from 'next/link';
 import HeadPage from './widgets/head';
 import "../css/main.css";
 import "../css/bootstrap.min.css";
-
+import MarkdownEditor from 'react-mde';
+import * as Showdown from 'showdown';
+import 'react-mde/lib/styles/css/react-mde-all.css';
 
 function Home () {
+  const markdownToHtmlConverter = new Showdown.Converter({
+    tables: true,
+    simplifiedAutoLink: true,
+    strikethrough: true,
+    tasklists: true
+  });
+  const [markdownContent, setMarkdownContent] = React.useState("");
+  const [selectedMardownMode, setSelectedMarkdownMode] = React.useState("write");
+
   return (
     <>
       <HeadPage></HeadPage>
@@ -23,6 +35,18 @@ function Home () {
       </ul>
 
       <h1>This is our homepage.</h1>
+
+      <div>
+        <MarkdownEditor 
+          value = {markdownContent}
+          onChange = {setMarkdownContent}
+          selectedTab = {selectedMardownMode}
+          onTabChange = {setSelectedMarkdownMode}
+          generateMarkdownPreview = {markdown => 
+            Promise.resolve(markdownToHtmlConverter.makeHtml(markdown))
+          }
+        />
+      </div>
     </>
   )
 }
