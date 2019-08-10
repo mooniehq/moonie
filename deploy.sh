178638fe29afc64cde1cdb92b9e19093364830ac
Blackhole02 <hangnguyen081995@gmail.com>
2019-08-11 01:57:31 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 set  -xe
 cd functions
-firebase --token 1/VYfiQZpTDv8RSVfSZYBeMGXQFwi-sq28jSphq_Eql-A functions:config:set service.db_username="postgres" service.db_password="dvt1KjoyBkCL47o0" service.db_host="104.197.58.229"
-firebase deploy --token 1/VYfiQZpTDv8RSVfSZYBeMGXQFwi-sq28jSphq_Eql-A
+echo $1
+firebase --token $1 functions:config:set service.db_username="postgres" service.db_password="dvt1KjoyBkCL47o0" service.db_host="104.197.58.229"
+firebase deploy --token $1
