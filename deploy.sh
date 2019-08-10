e3de4f1ac06beb73faf8c322e84db73ccf0b188b
Blackhole02 <hangnguyen081995@gmail.com>
2019-08-11 01:44:52 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 set  -xe
 firebase --project moonie --token 1/VYfiQZpTDv8RSVfSZYBeMGXQFwi-sq28jSphq_Eql-A functions:config:set service.db_username="postgres" service.db_password="dvt1KjoyBkCL47o0" service.db_host="104.197.58.229"
-firebase deploy --project moonie --token 1/VYfiQZpTDv8RSVfSZYBeMGXQFwi-sq28jSphq_Eql-A
+firebase deploy --only functions --project moonie --token 1/VYfiQZpTDv8RSVfSZYBeMGXQFwi-sq28jSphq_Eql-A
