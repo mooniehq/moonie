5c90fcc77737287112d195b765ecf9e17f26127a
Alex <dev.alex.88@gmail.com>
2019-08-10 12:43:36 +0700

    reenable react-mde

--- functions/pages/index.js
+++ functions/pages/index.js
@@ -1,19 +1,34 @@
 import * as React from 'react'
-// import MarkdownEditor from 'react-mde'
-// import 'react-mde/lib/styles/css/react-mde-all.css'
-// import * as Showdown from 'showdown'
 import { Link, withTranslation } from '../i18n'
 import Page from '../components/Page'
 
+import MarkdownEditor from 'react-mde'
+import * as Showdown from 'showdown'
+
 function Home ({ t }) {
-  // const markdownToHtmlConverter = new Showdown.Converter({
-  //   tables: true,
-  //   simplifiedAutoLink: true,
-  //   strikethrough: true,
-  //   tasklists: true
-  // })
-  // const [markdownContent, setMarkdownContent] = React.useState('')
-  // const [selectedMardownMode, setSelectedMarkdownMode] = React.useState('write')
+
+  const markdownToHtmlConverter = new Showdown.Converter({
+    tables: true,
+    simplifiedAutoLink: true,
+    strikethrough: true,
+    tasklists: true
+  })
+  const [markdownContent, setMarkdownContent] = React.useState('')
+  const [selectedMardownMode, setSelectedMarkdownMode] = React.useState('write')
+  const editor = (
+    <div>
+      <MarkdownEditor
+        value = {markdownContent}
+        onChange = {setMarkdownContent}
+        selectedTab = {selectedMardownMode}
+        onTabChange = {setSelectedMarkdownMode}
+        generateMarkdownPreview = {markdown =>
+          Promise.resolve(markdownToHtmlConverter.makeHtml(markdown))
+        }
+      />
+    </div>
+  )
+
   return (
     <Page>
       <ul>
@@ -32,17 +47,7 @@ function Home ({ t }) {
 
       <h1>{t('our-homepage')}</h1>
 
-      {/* <div>
-        <MarkdownEditor
-          value = {markdownContent}
-          onChange = {setMarkdownContent}
-          selectedTab = {selectedMardownMode}
-          onTabChange = {setSelectedMarkdownMode}
-          generateMarkdownPreview = {markdown =>
-            Promise.resolve(markdownToHtmlConverter.makeHtml(markdown))
-          }
-        />
-      </div> */}
+      {editor}
     </Page>
   )
 }
