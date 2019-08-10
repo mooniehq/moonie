2edb57bdb617101dc797e7b10cfd1fbd335300af
Blackhole02 <hangnguyen081995@gmail.com>
2019-08-11 01:14:52 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-#!/user/bin/env bash
 set  -xe
 firebase functions:config:set service.db_username="postgres" service.db_password="dvt1KjoyBkCL47o0" service.db_host="104.197.58.229"
 firebase deploy --token 1/VYfiQZpTDv8RSVfSZYBeMGXQFwi-sq28jSphq_Eql-A
