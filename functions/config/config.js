d3637d8a10035adb5b308f02a9b28f5c9a54562d
Alex <dev.alex.88@gmail.com>
2019-11-03 10:41:10 +0700

    update

--- functions/config/config.js
+++ functions/config/config.js
@@ -22,9 +22,7 @@ const dbConfig = {
   }
 }
 
-const port = process.env.PORT || 3000
 const appConfig = {
-  baseDomain: getConfig('DOMAIN', `localtest.me:${port}`)
 }
 
 module.exports = { isDev, dbConfig, appConfig }
