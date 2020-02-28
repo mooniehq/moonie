7c7c2ad111e8de451f8131dd445780c06bd2c9a0
Alex <dev.alex.88@gmail.com>
2020-02-28 15:46:27 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,37 @@
+/* eslint-disable import/no-webpack-loader-syntax */
 /**
  *  Magic file for next-i18next
  *
  */
-
 import React from 'react'
 
 import App from 'next/app'
 import { appWithTranslation } from '../i18n'
+import dynamic from 'next/dynamic'
 
 import 'easymde/dist/easymde.min.css'
-import '../semantic/dist/semantic.min.css'
+import '../semantic/dist/semantic.css'
 import '../css/tailwind.css'
+
+import('expose-loader?$!jquery')
+import('expose-loader?jQuery!jquery')
+
+const DynamicComponentWithNoSSR = dynamic(
+  () => {
+    import('../semantic/dist/semantic.js')
+    return <div></div>
+  },
+  { ssr: false }
+)
 
 class MyApp extends App {
   render () {
     const { Component, pageProps } = this.props
     return (
-      <Component {...pageProps} />
+      <>
+        <DynamicComponentWithNoSSR />
+        <Component {...pageProps} />
+      </>
     )
   }
 }
