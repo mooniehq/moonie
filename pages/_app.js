354ce0857bab0c8ed6617a92734299c7133ee37c
Alex <dev.alex.88@gmail.com>
2020-02-29 18:28:53 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -8,31 +8,31 @@ import React from 'react'
 
 import App from 'next/app'
 import { appWithTranslation } from '../i18n'
-import dynamic from 'next/dynamic'
+// import dynamic from 'next/dynamic'
 
 import 'easymde/dist/easymde.min.css'
 import '../semantic/dist/semantic.css'
 import '../css/tailwind.css'
 
 
-const DynamicComponentWithNoSSR = dynamic(
-  () => {
-    import('expose-loader?$!jquery')
-    import('expose-loader?jQuery!jquery')
-    import('../semantic/dist/semantic.js')
-    return <></>
-  },
-  { ssr: false }
-)
+// const DynamicComponentWithNoSSR = dynamic(
+//   () => {
+//     import('expose-loader?$!jquery')
+//     import('expose-loader?jQuery!jquery')
+//     import('../semantic/dist/semantic.js')
+//     return <></>
+//   },
+//   { ssr: false }
+// )
 
 class MyApp extends App {
   render () {
     const { Component, pageProps } = this.props
     return (
-      <>
-        <DynamicComponentWithNoSSR />
+      // <>
+      //   <DynamicComponentWithNoSSR />
         <Component {...pageProps} />
-      </>
+      {/* </> */}
     )
   }
 }
